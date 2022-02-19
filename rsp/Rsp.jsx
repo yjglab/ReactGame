93d46b8b254bc28682e60bb8d6aa9ea0abe2afab
yjglab <yjgdesign@gmail.com>
2022-02-20 00:48:40 +0900

    rsp class component init

---
+++ rsp/Rsp.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+class Rsp extends Component {
+  state = {
+    result: "",
+    imgCoord: 0,
+    score: 0,
+  };
+  componentDidMount() {}
+  componentWillUnmount() {}
+  render() {
+    const { result, score, imgCoord } = this.state;
+    return (
+      <>
+        <div
+          id="computer"
+          style={{
+            background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`,
+          }}
+        />
+        <div>
+          <button id="rock" className="btn" onClick={this.onClickBtn("바위")}>
+            바위
+          </button>
+          <button
+            id="scissor"
+            className="btn"
+            onClick={this.onClickBtn("가위")}
+          >
+            가위
+          </button>
+          <button id="paper" className="btn" onClick={this.onClickBtn("보")}>
+            보
+          </button>
+        </div>
+        <div>{result}</div>
+        <div>현재 {score}점</div>
+      </>
+    );
+  }
+}
+export default Rsp;
---
+++ rsp/client.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Rsp from "./Rsp";
+
+ReactDOM.render(<Rsp />, document.querySelector("#root"));
