884deca0024daeac8f9f84c0b27f2d1233bc2e04
yjglab <yjgdesign@gmail.com>
2022-02-20 15:38:39 +0900

    rsp class component

--- rsp/Rsp.jsx
+++ rsp/Rsp.jsx
@@ -1,12 +1,76 @@
 import React, { Component } from "react";
+
+const rspCoords = {
+  rock: "0",
+  scissors: "-142px",
+  paper: "-284px",
+};
+const scores = {
+  rock: 1,
+  scissors: -1,
+  paper: 0,
+};
+const comChoice = (imgCoord) => {
+  return Object.entries(rspCoords).find(function (v) {
+    return v[1] === imgCoord;
+  })[0];
+};
 class Rsp extends Component {
   state = {
     result: "",
-    imgCoord: 0,
+    imgCoord: rspCoords.rock,
     score: 0,
   };
-  componentDidMount() {}
-  componentWillUnmount() {}
+  interval;
+  componentDidMount() {
+    this.interval = setInterval(this.changeHand, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+  changeHand = () => {
+    const { imgCoord } = this.state;
+    if (imgCoord === rspCoords.rock) {
+      this.setState({
+        imgCoord: rspCoords.scissors,
+      });
+    } else if (imgCoord === rspCoords.scissors) {
+      this.setState({
+        imgCoord: rspCoords.paper,
+      });
+    } else if (imgCoord === rspCoords.paper) {
+      this.setState({
+        imgCoord: rspCoords.rock,
+      });
+    }
+  };
+  onClickBtn = (choice) => () => {
+    const { imgCoord } = this.state;
+    clearInterval(this.interval);
+    const myScore = scores[choice];
+    const comScore = scores[comChoice(imgCoord)];
+    const diff = myScore - comScore;
+    if (diff === 0) {
+      this.setState({
+        result: "비겼습니다",
+      });
+    } else if ([-1, 2].includes(diff)) {
+      this.setState((prevState) => {
+        return {
+          result: "이겼습니다",
+          score: prevState.score + 1,
+        };
+      });
+    } else {
+      this.setState((prevState) => {
+        return {
+          result: "졌습니다",
+          score: prevState.score - 1,
+        };
+      });
+    }
+    this.interval = setInterval(this.changeHand, 1000);
+  };
   render() {
     const { result, score, imgCoord } = this.state;
     return (
@@ -18,17 +82,17 @@ class Rsp extends Component {
           }}
         />
         <div>
-          <button id="rock" className="btn" onClick={this.onClickBtn("바위")}>
+          <button id="rock" className="btn" onClick={this.onClickBtn("rock")}>
             바위
           </button>
           <button
             id="scissor"
             className="btn"
-            onClick={this.onClickBtn("가위")}
+            onClick={this.onClickBtn("scissors")}
           >
             가위
           </button>
-          <button id="paper" className="btn" onClick={this.onClickBtn("보")}>
+          <button id="paper" className="btn" onClick={this.onClickBtn("paper")}>
             보
           </button>
         </div>
